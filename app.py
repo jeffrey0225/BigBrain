81f48b7af71e11a21d5b654b036e5cf520e8e259
Jeffrey <jeffreygood1234@gmail.com>
2022-01-01 22:26:24 +0800

    test

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from typing import Text
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_cors import cross_origin
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,52 +21,68 @@ class Todo(db.Model):
 
 
 @app.route('/', methods=['POST', 'GET'])
+@cross_origin()
 def index():
     if request.method == 'POST':
-        monkey = request.get_json()
-        print(monkey)
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        # monkey = request.get_json()
+        # print(monkey)
+        # task_content = request.form['content']
+        # new_task = Todo(content=task_content)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
+        # try:
+        #     db.session.add(new_task)
+        #     db.session.commit()
+        #     return redirect('/')
+        # except:
+        #     return 'There was an issue adding your task'
+        return '123456'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        # tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html')
 
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
+#  氣氛燈
+@app.route('/light', methods=['GET', 'POST'])
+def ligth():
 
     if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
+        return '32'
     else:
-        return render_template('update.html', task=task)
+        return render_template('light.html')
+
+# 窗簾動作
+@app.route('/curtain', methods=['GET', 'POST'])
+def curtain():
+    if request.get_json() != None:
+        state = request.get_json()
+    if request.method == 'POST':
+        print(request.get_json())
+        print('to curtain')
+        if state == 1:
+            print('open')
+            return '1'
+        if state == 2:
+            print('close')
+            return '2'
+    else:
+        return 'nothing happens'
+
+# 切換頁面
+@app.route('/curtain_html', methods=['GET', 'POST'])
+def curtain_html():
+
+    if request.method == 'POST':
+        return '2'
+    else:
+        return render_template('curtain.html')
+
+@app.route('/light_html', methods=['GET', 'POST'])
+def ligth_html():
+
+    if request.method == 'POST':
+        return '32'
+    else:
+        return render_template('light.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True,host='172.20.10.2',port=8000)
+# 執行網站
+app.run(debug=True,host='0.0.0.0',port=8000)
---
+++ static/script_curtain.js
@@ -0,0 +1,46 @@
+var open_1 = document.getElementById('open');
+var close_1 = document.getElementById('close');
+var woke = document.getElementById('woke');
+var bed = document.getElementById('bed');
+
+const sendHttpRequest = (method, url, data) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+  
+    xhr.responseType = 'json';
+    
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    xhr.send(JSON.stringify(data));
+    
+};
+
+const sendData_open = () => {
+    sendHttpRequest('POST', '/curtain', 1, {
+    })
+};
+const sendData_close = () => {
+    sendHttpRequest('POST', '/curtain', 2, {
+    })
+};
+const sendData_woke = () => {
+    sendHttpRequest('POST', '/curtain', 1, {
+    })
+    sendHttpRequest('POST', '/light', 5, {
+    })
+};
+const sendData_bed = () => {
+    sendHttpRequest('POST', '/curtain', 2, {
+    })
+    sendHttpRequest('POST', '/light', 6, {
+    })
+};
+
+
+
+open_1.addEventListener('click', sendData_open);
+close_1.addEventListener('click', sendData_close);
+woke.addEventListener('click', sendData_woke);
+bed.addEventListener('click', sendData_bed);
+
+
